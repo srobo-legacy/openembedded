f703c983102d5888c0a2993460e8a229bfc2c4d5
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2009-08-22 16:28:03 +0200

    Introduce a cleaner way to build the kernel

---
+++ recipes/linux/linux-2.6.30/at91/make-at91-symlinks.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+ln -s	at91sam9260ek		at91sam9260df
+ln -s	at91sam9260ek		at91sam9260dfc
+ln -s	at91sam9260ek		at91sam9260nf
+
+ln -s	at91sam9261ek		at91sam9261df
+
+ln -s	at91sam9263ek		at91sam9263dfc
+
+ln -s	at91sam9g10ek		at91sam9g10df
+ln -s	at91sam9g10ek		at91sam9g10nf
+
+ln -s	at91sam9g20ek		at91sam9g20df
+ln -s	at91sam9g20ek		at91sam9g20dfc
+ln -s	at91sam9g20ek		at91sam9g20nf
+
+ln -s	at91sam9g45ek		at91sam9g45df
+ln -s	at91sam9g45ek		at91sam9g45nf
+ln -s	at91sam9g45ek		at91sam9g45dfes
+ln -s	at91sam9g45ek		at91sam9g45ekes
+ln -s	at91sam9g45ek		at91sam9g45nfes
+ln -s	at91sam9g45ek		at91sam9m10df
+ln -s	at91sam9g45ek		at91sam9m10ek
+ln -s	at91sam9g45ek		at91sam9m10nf
+
+ln -s	at91sam9xeek		at91sam9xedf
+ln -s	at91sam9xeek		at91sam9xedfc
+ln -s	at91sam9xeek		at91sam9xenf
+
